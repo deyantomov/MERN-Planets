9558f4de69548e58ce2c67dbe077b94340057fa4
deyantomov <deyan.tomov.a58@learn.telerikacademy.com>
2024-07-03 09:10:01 +0300

    feat: add redux devtools

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,10 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import coordinateReducer from './features/coordinates/coordinateSlice'; // Adjust the import path as necessary
+import coordinateReducer from './features/coordinates/coordinateSlice';
 
 export const store = configureStore({
   reducer: {
     coordinates: coordinateReducer,
   },
+  devTools: true,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
